58824919b8c1b33bd98f2e8e93c98d9dfec3e6fd
Daniel <danielibukun2000@gmail.com>
2025-08-29 23:19:16 +0100

    Third Commit

--- lib/addScreen.dart
+++ lib/addScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_plus/iconsax_plus.dart';
 
 class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
@@ -14,9 +15,153 @@ class _AddScreenState extends State<AddScreen> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 20),
         decoration: BoxDecoration(
           gradient: RadialGradient(
             colors: [Color(0xffFFB899), Color(0xffA8E3DC)],
+          ),
+        ),
+        child: Center(
+          child: Container(
+            height: 560,
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color.fromARGB(211, 255, 255, 255), Color(0xffEFF0F6)],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Add Your Pet",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontFamily: "Azonix",
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "Enter the details of your Pet",
+                  style: TextStyle(
+                    fontFamily: "Inter",
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xffA8E3DC),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                      hintText: "Pet's Name",
+                      hintStyle: TextStyle(
+                        fontSize: 16,
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                      prefixIcon: Icon(
+                        IconsaxPlusBold.pet,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xffA8E3DC),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                      hintText: "Pet's Breed",
+                      hintStyle: TextStyle(
+                        fontSize: 16,
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                      prefixIcon: Icon(
+                        IconsaxPlusBold.pet,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xffA8E3DC),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                      hintText: "Pet's Description",
+                      hintStyle: TextStyle(
+                        fontSize: 16,
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                      ),
+                      prefixIcon: Icon(
+                        IconsaxPlusBold.pet,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Container(
+                  height: 65,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xffE45A2A),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(IconsaxPlusBold.camera, color: Colors.white),
+                      SizedBox(width: 20),
+                      Text(
+                        "UPLOAD PICTURE",
+                        style: TextStyle(
+                          fontFamily: "Inter",
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
