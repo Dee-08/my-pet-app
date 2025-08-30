51fbda8f25633ad5010e75c72c797baad1d2a38a
Daniel <danielibukun2000@gmail.com>
2025-08-30 13:58:23 +0100

    Seventh Commit

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 5), () {});
+
     return Scaffold(
       body: Container(
         height: double.infinity,
         width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xffA8E3DC), Color(0xffFFB899)],
+          ),
+        ),
         child: Center(
           child: Image.asset(
             "assets/images/logo_bg.png",
-            height: 250,
-            width: 250,
+            height: 320,
+            width: 320,
           ),
         ),
       ),
